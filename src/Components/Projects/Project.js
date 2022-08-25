0a89bfba0b07bad92205bcd313c336a666ced4b8
PanditUtkarsh <panditutkarsh555@gmail.com>
2022-08-25 12:14:20 +0530

    adding projects

--- src/Components/Projects/Project.js
+++ src/Components/Projects/Project.js
@@ -6,10 +6,9 @@ function Project() {
     <div className="projct">
       <h3 className="projct_h3">Projects</h3>
       <div className="projct_flex">
-      <a href="https://musicappbypanditutkarsh.netlify.app/"><div id="projct_1" className="projct_card"><img src="" alt="" /></div></a>
-      <div id="projct_2" className="projct_card"><a href="https://random-quotes-generator-pandit-utkarsh.netlify.app/"><img src="" alt="" /></a></div>
-      <div id="projct_3" className="projct_card"><a href="https://just-book-utkarsh.netlify.app/"><img src="" alt="" /></a></div>
-      <div id="projct_4" className="projct_card"><a href=""><img src="" alt="" /></a></div>
+      <a target="_blank" className="projects_a" href="https://musicappbypanditutkarsh.netlify.app/"><div id="projct_1" className="projct_card"></div><p id="music_plyr_p">Music player</p></a>
+      <a target="_blank" className="projects_a" href="https://random-quotes-generator-pandit-utkarsh.netlify.app/"><div id="projct_2" className="projct_card"></div><p id="music_plyr_p">Random Quotes </p></a>
+      <a target="_blank" className="projects_a" href="https://just-book-utkarsh.netlify.app/"><div id="projct_3" className="projct_card"></div><p id="music_plyr_p">Movie Booking</p></a>
       </div>
     </div>
   );
